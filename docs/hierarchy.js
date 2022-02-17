2ab13bb84f954cb980da7f4cec825709bb89eb4e
Docs Robot <robot@example.com>
2022-02-17 12:12:18 +0000

    Committing documentation changes

--- docs/hierarchy.js
+++ docs/hierarchy.js
@@ -1,6 +1,7 @@
 var hierarchy =
 [
-    [ "SLIDER_POTENTIOMETER_EASYC::EasyC", "class_s_l_i_d_e_r___p_o_t_e_n_t_i_o_m_e_t_e_r___e_a_s_y_c_1_1_easy_c.html", [
+    [ "EasyC", null, [
       [ "sliderPot", "classslider_pot.html", null ]
-    ] ]
+    ] ],
+    [ "SLIDER_POTENTIOMETER_EASYC::EasyC", "class_s_l_i_d_e_r___p_o_t_e_n_t_i_o_m_e_t_e_r___e_a_s_y_c_1_1_easy_c.html", null ]
 ];
